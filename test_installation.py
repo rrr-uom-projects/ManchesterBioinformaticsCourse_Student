5be0cd25338b6cbfc836f4191b7ad1169a29cb83
Andrew Green <a.f.green1@gmail.com>
2021-01-11 13:53:35 +0000

    Think this checks all the required libraries now

--- test_installation.py
+++ test_installation.py
@@ -11,15 +11,45 @@ except:
 try:
     import matplotlib
 except:
+    print("Matplotlib not found, please run:")
     print("python -m pip install matplotlib")
 
 try:
     import jupyter
 except:
+    print("jupyter not found, please run:")
     print("python -m pip install jupyter")
 
 
 try:
     import scipy
 except:
+    print("scipy not found, please run:")
     print("python -m pip install scipy")
+
+try:
+    import skimage
+except:
+    print("scikit image not found, please run:")
+    print("python -m pip install scikit-image")
+
+try:
+    import pydicom
+except:
+    print("pydicom not found, please run:")
+    print("python -m pip install pydicom")
+
+
+## below here, optional stuff that will only be used in the extensions
+
+try:
+    import pyaudio
+except:
+    print("pyaudio was not found, if you plan to work on the extension tasks, run this:")
+    print("python -m pip install pyaudio wave")
+
+try:
+    import colorama
+except:
+    print("colorama was not found, if you plan to work on the extension tasks, run this:")
+    print("python -m pip install colorama")
