c0d3d13360db474a7f157375934a30b5b58001c1
Andrew Green <a.f.green1@gmail.com>
2021-01-11 13:16:18 +0000

    Update installation tester script, not finished yet

--- test_installation.py
+++ test_installation.py
@@ -8,4 +8,18 @@ except:
     print("Numpy was not found.")
     print("Install it by doing: python -m pip install numpy ")
 
+try:
+    import matplotlib
+except:
+    print("python -m pip install matplotlib")
 
+try:
+    import jupyter
+except:
+    print("python -m pip install jupyter")
+
+
+try:
+    import scipy
+except:
+    print("python -m pip install scipy")
