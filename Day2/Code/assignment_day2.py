29a98bb50c9d3d7b0fb723c0a79288e417d09b66
meganpaynton59@hotmail.com <meganpaynton59@hotmail.com>
2021-01-19 16:22:23 +0000

    assignment 2 commit 3

--- Day2/Code/assignment_day2.py
+++ Day2/Code/assignment_day2.py
@@ -12,8 +12,35 @@ print(p01_data)
 
 print(p01_data[0])
 
-plt.plot(p01_data[0], p01_data[1])
-plt.show()
-
-#plt.scatter(p01_data)
+# plot raw data
+#plt.plot(p01_data[0], p01_data[1])
 #plt.show()
+
+#plt.scatter(p01_data[0], p01_data[1])
+#plt.show()
+
+#convolve data
+xValues = p01_data[0]
+N = int(xValues.shape[0]/32)
+
+window = np.ones(N)
+convolved = np.convolve(window/window.sum(), p01_data[1], mode='same')
+
+#plt.plot(xValues, convolved)
+#plt.show()
+
+#find peaks 
+peaks_p01 = find_peaks(convolved)
+print(peaks_p01)
+
+peaks = np.asarray(peaks_p01)
+print(peaks)
+print(type(peaks))
+print(xValues[peaks])
+
+#plot raw data and convoluted data
+#plt.plot(p01_data[0], p01_data[1], color='k')
+#plt.plot(xValues, convolved, color='r')
+#plt.scatter(xValues[peaks_p01], peaks, colour='g')
+#plt.show()
+
