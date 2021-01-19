0da81249002201ff5d4dd82f9e86ba270150bd5f
meganpaynton59@hotmail.com <meganpaynton59@hotmail.com>
2021-01-19 15:44:28 +0000

    assignement 2 commit 2

--- Day2/Code/assignment_day2.py
+++ Day2/Code/assignment_day2.py
@@ -1,13 +1,19 @@
 # Aim is to plot convoluted respiratory data
 
 # import required modules
-import numpy as np
 import matplotlib.pyplot as plt
-import scipy.signal
+from scipy.signal import find_peaks
+import numpy as np
 
 # read first csv file
-p01_data = np.loadtxt("breathing_data/p01.csv", dtype='int,int', delimiter=',', usecols=(0,1), unpack=True)
+p01_data = np.loadtxt("breathing_data/p01.csv", delimiter=',')
 
 print(p01_data)
 
-test_plot = plt.plot(p01_data)
+print(p01_data[0])
+
+plt.plot(p01_data[0], p01_data[1])
+plt.show()
+
+#plt.scatter(p01_data)
+#plt.show()
