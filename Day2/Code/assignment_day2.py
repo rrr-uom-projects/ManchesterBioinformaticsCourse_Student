4dfcb368ea54805162fc23d70b08d90aa41bad27
AliJ752 <alistair.jones3@nhs.net>
2021-01-19 17:16:06 +0000

    For loop and added images

--- Day2/Code/assignment_day2.py
+++ Day2/Code/assignment_day2.py
@@ -12,20 +12,27 @@ for you.
 #import modules
 import numpy as np
 import matplotlib.pyplot as plt
+from os import listdir
 from scipy.signal import find_peaks
 #open first csv
 #loads an individual csv and tells loadtxt that this file is delimited by ","
-
-p01 = np.loadtxt(r"/home/ali/git_repos/ICT_module/ManchesterBioinformaticsCourse_Student/Day2/Code/breathing_data/p01.csv", delimiter=",")
-# Moving averages window function - ours
-N = int(p01[0,:].shape[0]/40) # Try changing the window width to see the effect on the filtered signal
-window = np.ones(N)
-convolved = np.convolve(window/window.sum(), p01[1,:], mode='same')# Note - divide by the sum of the window to 
-# maintain normalisation
-peaks, _ = find_peaks(convolved, distance=50)
-plt.plot(p01[0,:], p01[1,:], linewidth=2)
-plt.plot(p01[0,:], convolved, color='black')
-plt.plot(peaks, convolved[peaks], "x", color='r')
+file_list = listdir('/home/ali/git_repos/ICT_module/ManchesterBioinformaticsCourse_Student/Day2/Code/breathing_data/')
+contents = []
+for file_name in file_list:
+    p01 = np.loadtxt(r"/home/ali/git_repos/ICT_module/ManchesterBioinformaticsCourse_Student/Day2/Code/breathing_data/" + file_name, delimiter=",")
+    # Moving averages window function - ours
+    N = int(p01[0,:].shape[0]/40) # Try changing the window width to see the effect on the filtered signal
+    window = np.ones(N)
+    convolved = np.convolve(window/window.sum(), p01[1,:], mode='same')# Note - divide by the sum of the window to 
+    # maintain normalisation
+    fig = plt.figure(figsize=(16,9))
+    peaks, _ = find_peaks(convolved, distance=50)
+    plt.plot(p01[0,:], p01[1,:], linewidth=2)
+    plt.plot(p01[0,:], convolved, color='black')
+    plt.plot(peaks, convolved[peaks], "x", color='r')
+    savefilename = (str(file_name) + '.png')
+    print(savefilename)
+    plt.savefig(savefilename)
 
 
 
