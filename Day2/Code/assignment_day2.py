e347c4db38504839df02af1279de34583702e95a
lkcnlkcn <longki.chan@uhb.nhs.uk>
2021-01-19 17:05:57 +0000

    Update assignment_day2.py

--- Day2/Code/assignment_day2.py
+++ Day2/Code/assignment_day2.py
@@ -3,3 +3,9 @@ Refer to Practical2_instructions.pdf for a guide
 
 Remember - comments are not optional
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks 
+
+
