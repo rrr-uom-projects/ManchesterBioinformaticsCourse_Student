819aad929c25e7fae0904db2f1a3bd78ea1933f4
meganpaynton59@hotmail.com <meganpaynton59@hotmail.com>
2021-01-19 15:18:14 +0000

    first commit assignement 2

--- Day2/Code/assignment_day2.py
+++ Day2/Code/assignment_day2.py
@@ -1,5 +1,13 @@
-"""
-Refer to Practical2_instructions.pdf for a guide
+# Aim is to plot convoluted respiratory data
 
-Remember - comments are not optional
-"""
+# import required modules
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal
+
+# read first csv file
+p01_data = np.loadtxt("breathing_data/p01.csv", dtype='int,int', delimiter=',', usecols=(0,1), unpack=True)
+
+print(p01_data)
+
+test_plot = plt.plot(p01_data)
