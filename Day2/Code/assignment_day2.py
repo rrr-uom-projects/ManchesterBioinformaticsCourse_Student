826ef6e17392d6ecbf4a5322968285965a8ac06e
jj-l98 <71712407+jj-l98@users.noreply.github.com>
2021-01-20 10:47:02 +0000

    Update assignment_day2.py

--- Day2/Code/assignment_day2.py
+++ Day2/Code/assignment_day2.py
@@ -5,24 +5,31 @@ Remember - comments are not optional
 """
 print("Hello World")
 
-#Change working directory
-os.chdir("C:/Users/jaden/bioinformatics-course/code/ManchesterBioinformaticsCourse_Student/Day2/Code/breathing_data")
-os.getcwd()
 #importing required packages and modules
 import numpy as np
+import matplotlib
+import scipy
+matplotlib.use('TKAgg')
+matplotlib.interactive(True)
 import matplotlib.pyplot as plt
-import scipy.signal as sgnl
+import scipy
 import skimage
 import os
 import io
 
+#Change working directory
+os.chdir("C:/Users/jaden/bioinformatics-course/code/ManchesterBioinformaticsCourse_Student/Day2/Code/breathing_data")
+os.getcwd()
+
+
 #Use numpy.loadtxt to load the first csv file.
 file = open("p01.csv")
-data = np.loadtxt(file, delimited=",")
-x,y = np.loadtxt(file, delimiter=",")
+data = np.loadtxt(file, delimiter=",")
+#x,y = np.loadtxt(file, delimiter=",")
+x, y = data
 x
 y
-data
+
 #print(data)
 
 #Check the documentation for this function to figure out how to load a csv.
@@ -54,13 +61,58 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Interesting Graph\nCheck it out')
 plt.legend()
+plt.draw()
 plt.show()
 
 #Creating scatterplot for p01.csv data:
-plt.scatter(data)
+plt.scatter(x, y)
 plt.show()
 
+type(x)
 
+#Convolving data
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Plot the Raw Data
+ts = rawdata[0:500]
+plt.plot(ts, 'r-')
+plt.ylabel('Lightpower (V)')
 
+#    previous version
+#    smooth_data = pd.rolling_mean(rawdata[0:500],5).plot(style='k')
+#    changes to pandas require a change to the code as follows:
+smooth_data = pd.Series(ts).rolling(window=7).mean().plot(style='k')
+plt.show()
+
+    # Moving averages window function
+N = x.shape[0]/32 # Try changing the window width to see the effect on the filtered signal
+window = np.ones(N)
+convolved = np.convolve(window/window.sum(), sinWithRealNoise, mode='same')# Note - divide by the sum of the window to 
+                                                                        #    maintain normalisation
+plt.plot(x, convolved)
+plt.plot(x, sin4, linewidth=2)
+
+convolved = np.convolve(x, y) #or try mode=full 
+np.convolve(x, y)
+#data
+convolved
+plt.plot(x, convolved)
+plt.show()
+
+import scipy as sp
+import pandas as pd
+
+#N = xValues.shape[0]/32 # Try changing the window width to see the effect on the filtered signal
+#window = np.ones(N)
+#convolved = np.convolve(window/window.sum(), sinWithRealNoise, mode='same')# Note - divide by the sum of the window to 
+                                                                        #    maintain normalisation
+#plt.plot(xValues, convolved)
+#plt.plot(xValues, sin4, linewidth=2)
+
+#find peaks in graph
+#find_peaks(convolved)
+#scipy.signal.find_peaks(convolved)
+
+#Calculate peak to peak interval
 
