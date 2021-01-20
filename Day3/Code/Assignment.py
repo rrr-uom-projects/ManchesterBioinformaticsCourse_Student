52ea0c0bbb43e335df4445894e7cfb25c28e7e5e
kwi11iams <katie.williams-5@postgrad.manchester.ac.uk>
2021-01-20 13:51:57 +0000

    Katie commit 1, completed part 1 and 2 of pdf

--- Day3/Code/Assignment.py
+++ Day3/Code/Assignment.py
@@ -1,7 +1,15 @@
 """
-Refer to Assignment.pdf for instructions!
+BIOL68310 Assignment: Image processing in Python
 
-Remember - this is assessed. Make sure your code is nicely structured
+Authors: Katie Williams and Megan Paynton
 
-Comments are not optional
+Work done on Wednesday 20th January 2021
 """
+
+# First, import the relevant modules and functions
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.io import imread 
+
+# Load the file, CT.jpg
+
