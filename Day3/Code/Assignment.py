f8825bf2f0817ac2018bc78e5f22cb7b96acd78a
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2021-01-20 14:38:34 +0000

    completed to part 4

--- Day3/Code/Assignment.py
+++ Day3/Code/Assignment.py
@@ -2,7 +2,7 @@
 Assignment for Day 3
 Image Processing in Python
 
-Katherine Winfield (@kjwinfield) and Yongwon Ju (@ )
+Katherine Winfield (@kjwinfield) and Yongwon Ju (@yongwonju)
 
 This version written on 20 January 2021
 """
@@ -15,5 +15,10 @@ from skimage.io import imread
 
 #open the image
 image = io.imread("lungs.jpg")
-plt.imshow(image)
+plt.imshow(image) #displays the image using matplotlib
+plt.close()
+
+#generate a histogram showing the frequency of different intensities of pixel
+image_info = np.mean(image, -1)
+plt.hist(image_info[image_info > 1].flatten(), bins=254)
 plt.show()
