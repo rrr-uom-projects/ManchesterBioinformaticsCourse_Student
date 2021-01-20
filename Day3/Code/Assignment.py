77c8a0db202e0f000f6147bf763294a19148e118
helenajr <helenajrobinson@gmail.com>
2021-01-20 15:02:15 +0000

    day 3 work in progress

--- Day3/Code/Assignment.py
+++ Day3/Code/Assignment.py
@@ -5,3 +5,45 @@ Remember - this is assessed. Make sure your code is nicely structured
 
 Comments are not optional
 """
+
+"""We are Nermeen and Helena group. This is the first part of Day3 tasks. For this task, we need to load lungs.jpg image and plot 
+
+a histogram image. This allows us to identify the distribution of the values of the lungs.jpg image."""
+
+#Import the required libraries.
+import numpy as np #this library deals with the image as an array of numbers
+import matplotlib.pyplot as plt #this is used to plot the figures
+from skimage import io #this is used for image processing
+
+
+#Load the image
+image= io.imread('lungs.jpg')
+#You can use the following two lines to see your image before moving on (just remove the #).
+#plt.imshow(image)
+#plt.show()
+
+#To plot the figure we use matplotlib
+data = np.mean(io.imread("lungs.jpg"), -1) #this will take the mean of all channels. -1 refers to the channels
+plt.hist(data[data > 1].flatten(), bins=254)#flatten converts the image into a 
+#1D array then a histogram is plotted with 254 bins to reflect each possible gray value
+#[data > 1] is used because there LOADS of pixels with the value 0
+
+plt.show()
+#print the image after modification
+print(np.min(image), np.max(image))
+#Here you can display your histogram
+plt.imshow(image, cmap="Greys_r", vmin=0.0, vmax=1.0)
+#To see your original image before modificaiton
+plt.show()
+
+"""
+Explaining the histogram - the histogram shows the number of pixels of each 
+possible gray value (intensity). y axis = number of pixels, x axis = possible pixel values (intensity)
+The x axis goes from 1 to 255 where 1 = almost-black and 255 = white
+"""
+
+def level_transform (window, level):
+    
+
+
+
--- Day3/Code/Helena_draft.py
+++ Day3/Code/Helena_draft.py
@@ -12,12 +12,32 @@ from skimage.io import imread #Is this line correct?
 from PIL import Image
 
 #load image
-image = imread('lungs.jpg')
+image = np.mean(imread("lungs.jpg"), -1)
+#image = imread('lungs.jpg', as_gray=True)
 print(f"image = {image}")
-
+plt.hist(data[data >1].flatten(), bins 254)
 #display image using function from pillow module
-img = Image.open('lungs.jpg')
-img.show()
+#img = Image.open('lungs.jpg')
+#img.show()
 
-#plot a histogram of the image
+#plt.hist(image.flatten(), bins=256, range=(0, 1))
+#bins=256, range=(0, 1)
+
+
+#create a histogram of the image - gives a 1d array of number of pixels of
+#each possible gray value
+"""
 histogram, bin_edges = np.histogram(image, bins=256, range=(0,1))
+
+
+
+plt.figure()
+plt.title("Grayscale Histogram")
+plt.xlabel("grayscale value")
+plt.ylabel("pixels")
+plt.xlim([0.0, 1.0])  # <- named arguments do not work here
+
+plt.plot(bin_edges[0:-1], histogram)  # <- or here
+plt.show()
+
+print("Done")
