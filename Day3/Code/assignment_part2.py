40d437d6dd27a4c9a7af9fc689bd085877c5feb5
Viveck Kingsley <viveckkingsley@Vivecks-MacBook-Pro.local>
2021-01-21 14:36:10 +0000

    part 2 done!

--- Day3/Code/assignment_part2.py
+++ Day3/Code/assignment_part2.py
@@ -34,7 +34,7 @@ Overlaying images in the same axis:
 
 fig = plt.figure() # Create Figure using matplotlib.
 ax = fig.add_subplot(111) # Creating plot - (111) means the creation of a 1x1 grid, plot 1.
-""" Reverse greyscale colour map was used for easy visualisation of the appropriate tissue. """"
+#""" Reverse greyscale colour map was used for easy visualisation of the appropriate tissue. """"
 ax.imshow(image, cmap="Greys_r")  # Image on axis, with colormap Greys_r used.
 floating = ax.imshow(image3, cmap="Greys_r", alpha=0.5) # Alpha = 0.5 to make image 3 semi transparent (0.5).
 
@@ -44,38 +44,39 @@ _______________________
 Shift_Function to translate/rotate image 3 onto image:
 """
 
-""" shifts argument allows for manual selection of movement in x and y planes, as well as r (=rotation), which allows for rotation of image 3 """"
+#""" shifts argument allows for manual selection of movement in x and y planes, as well as r (=rotation), which allows for rotation of image 3 """"
 def shift_image(shifts, r):
     global image3
 #    ax1.imshow(image, cmap="Greys_r", alpha = 1) 
 # .shift function of image 3 is assigned to "shifting" variable, and functions by translating image 3 with shifts[0] and shifts[1].
     shifting = interpolation.shift(image3, (shifts[0], shifts[1]), mode="nearest") 
-    rotating = interpolation.rotate(shifting, r, reshape=False)
+    image3 = interpolation.rotate(shifting, r, reshape=False)
 #    ax1.imshow(floating, cmap="Greys_r", alpha = 0.5)
-    floating.set_data(rotating)
+    floating.set_data(image3)
     fig.canvas.draw()
     
-# shift_image([y, x], r)
+#shift_image([-100, 20], 1)
+#plt.show()
 
 def eventHandler(event):
     up = 0
-    left = 0
-    right = 0
-    down = 0
+    down = 1
     whichKey=event.key
     print(whichKey)
-    if whichKey == "up":
-        shift_image([-100, 0], 0)
-    elif whichKey == "left":
-        shift_image == ([0, 100], 0)
-    elif whichKey == "down":
-        shift_image == ([-100, 0], 0)
-    elif whichKey == "right":
-        shift_image == ([100, 0], 0)
-    elif whichKey == "r":
-        shift_image == ([0, 0], 100)
-    elif whichKey == "t":
-        shift_image == ([0, 0], -100)
+    if whichKey == 'up':
+        up = 1
+        shift_image([-1, 0], 0)
+        print (up)
+    elif whichKey == 'left':
+        shift_image([0, 1], 0)
+    elif whichKey == 'down':
+        shift_image([1, 0], 0)
+    elif whichKey == 'right':
+        shift_image([0, -1], 0)
+    elif whichKey == 'r':
+        shift_image([0, 0], 1)
+    elif whichKey == 't':
+        shift_image([0, 0], -1)
     
 fig.canvas.mpl_connect('key_press_event', eventHandler)
 
