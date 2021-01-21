686e7c6997bc8efc74fc9428bcc80798f646051e
jj-l98 <71712407+jj-l98@users.noreply.github.com>
2021-01-21 23:36:44 +0000

    Create Day 4 Assignment.py

---
+++ Day4/Code/Day 4 Assignment.py
@@ -0,0 +1,19 @@
+# Day 4 Assignment
+## Name: Jadene and Long-Ki
+## Date: 21/01/21
+## Title: Image processing in Python
+
+## PART 1 ##
+
+# Import relevant modules and libraries
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy 
+from scipy.ndimage import interpolation, rotate 
+import os
+import skimage.io 
+import skimage
+from skimage import io
+import io 
+import matplotlib.image as mpimg 
