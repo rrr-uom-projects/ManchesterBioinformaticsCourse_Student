86e54fa78e4d3ad1f5ce6b28013f7c07023060ef
Ruth <ruthkeane166@gmail.com>
2021-01-21 00:43:51 +0000

    part 2 interactive- more reusable code (new LungNew variable)

--- Day3/Code/Assignment_part2.py
+++ Day3/Code/Assignment_part2.py
@@ -15,7 +15,7 @@ from scipy.ndimage import interpolation
     
 def shiftImage(the_shifts): 
     """
-    Shifts Lungs2 image in the xy plane. 
+    Shifts LungsNew image in the xy plane. 
 
     Parameters
     ----------
@@ -27,19 +27,20 @@ def shiftImage(the_shifts):
     None.
 
     """
-    # bring in Lungs2 image (global so as can be edited)
-    global Lungs2
-    # Shift Lungs2 image using interpolation
+    # bring in LungsNew image (global so as can be edited)
+    global LungsNew
+    # Shift LungsNew image using interpolation
     # Shifts are those from function arguments, move image in xy plane
-    Lungs2 = interpolation.shift(Lungs2, shift = the_shifts)
+    LungsNew = interpolation.shift(LungsNew, shift = the_shifts)
     # Updates floating image in figure space
-    floating.set_data(Lungs2)
+    floating.set_data(LungsNew)
     # Draws update to canvas
     fig.canvas.draw()
 
+
 def shiftRotateImage(the_shifts,rotations): 
     """
-    Shifts and rotates Lungs3 image in the xy plane.
+    Shifts and rotates LungsNew image in the xy plane.
 
     Parameters
     ----------
@@ -52,35 +53,74 @@ def shiftRotateImage(the_shifts,rotations):
     None.
 
     """
-    # bring in Lungs3 image (global so as can be edited)
-    global Lungs3
-    # Shift Lungs3 image using interpolation
+    # bring in LungsNew image (global so as can be edited)
+    global LungsNew
+    # Shift LungsNew image using interpolation
     # Shifts are those from function arguments, move image in xy plane
-    Lungs3 = interpolation.shift(Lungs3, shift = the_shifts)
+    LungsNew = interpolation.shift(LungsNew, shift = the_shifts)
     # Rotates Lung3 image using interpolation
     # Roation is from function argument, rotate in xy plane
-    Lungs3 = interpolation.rotate(Lungs3, angle=rotations, reshape=False)
+    LungsNew = interpolation.rotate(LungsNew, angle=rotations, reshape=False)
     # Updates floating2 image in figure space
-    floating2.set_data(Lungs3)
+    floating.set_data(LungsNew)
     # Draws update to canvas (for figure 2)
-    fig2.canvas.draw()
+    fig.canvas.draw()
+
+def eventHandler(event):
+    """
+    This function calls shiftRotateImage each time a key is pressed, shifting or rotating the image by one unit. 
+    The key pressed determines which movement occurs
+    The changes made are used to modify the dictionary to keep track of changes happening
+    Parameters
+    ----------
+    event : character
+        key that has been pressed        
+    Returns
+    -------
+    None.
+    """
+    #find the key pressed for the event
+    whichKey = event.key
+    #6 key options here: up, down, left, right cause shifts. o and p rotate to the left/riht respectively
+    #the movement_storage dictionary is a global variable and is modified each time a relevant key is pressed
+    if whichKey == 'up':
+        shiftRotateImage([-1,0],0)
+        movement_storage["x"]=movement_storage["x"]-1
+    if whichKey == 'down':
+        shiftRotateImage([1,0],0)
+        movement_storage["x"]=movement_storage["x"]+1
+    if whichKey == 'left':
+        shiftRotateImage([0,-1],0)
+        movement_storage["y"]=movement_storage["y"]-1
+    if whichKey == 'right':
+        shiftRotateImage([0,1],0)  
+        movement_storage["y"]=movement_storage["y"]+1
+    if whichKey == 'p':
+        shiftRotateImage([0,0],-1)  
+        movement_storage["rotation"]=movement_storage["rotation"]-1
+    if whichKey == 'o':
+        shiftRotateImage([0,0],1)
+        movement_storage["rotation"]=movement_storage["rotation"]+1
 
 def main():
     """
-    Makes plots for Lung2 registered to Lung1 and Lung3 registered to Lung1
+    Makes plots for Lungs2 registered to Lungs1 and Lungs3 registered to Lung1
+    Makes plots for Lung1 and Lung 3, allowing user to register them using keystrokes
+    Outputs changes made once image is closed
     """
-    #make Lungs 2, floating and fig global so they can be accessed within shiftImage.
-    #make Lungs 3, floating2 and fig2 global so they can be accessed within shiftImageRotate
-    global Lungs2   
-    global Lungs3
+    #make LungsNew, floating and fig global so they can be accessed within shiftImage and ShiftImageRotate.
+    global LungsNew   
     global floating
-    global floating2 
     global fig
-    global fig2
-    # Read data
+    #make movement_storage global so it can be accessed in eventHandler
+    global movement_storage
+    
+    # Read data for lungs1
     Lungs1 = imread("Lungs.jpg", as_gray = True)
-    Lungs2 = imread("Lungs2.jpg", as_gray = True)
-    Lungs3 = imread("Lungs3.jpg", as_gray = True)
+    
+    ## lungs2 image registration
+    # Read data for lungs2
+    LungsNew = imread("Lungs2.jpg", as_gray = True)
     #Create figure space and add subplots for shift plot
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
@@ -89,28 +129,49 @@ def main():
     # Colour images using cmap so as to view more easily on screen 
     # Use alpha = 0.5 to make second image transparent
     ax1.imshow(Lungs1, cmap = "Greys_r")
-    floating = ax2.imshow(Lungs2, alpha = 0.5, cmap = "Reds")    
+    floating = ax2.imshow(LungsNew, alpha = 0.5, cmap = "Reds")    
     # Use shift image to find the shift required to register the images.
     # Shift required is [-16, -40]
     shiftImage([-16, -40])  
-
-
+    
+    ### lungs3 image registration
+    #read in lungs3 image
+    LungsNew = imread("Lungs3.jpg", as_gray = True)
     #Create figure space and add subplots for shift and rotation plot
-    fig2 = plt.figure()
-    ax1b = fig2.add_subplot(111)
-    ax2b = fig2.add_subplot(111)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax2 = fig.add_subplot(111)
     # Colour images using cmap so as to view more easily on screen 
     # Use alpha = 0.5 to make second image transparent
-    ax1b.imshow(Lungs1, cmap = "Greys_r")
-    floating2 = ax2b.imshow(Lungs3, alpha = 0.5, cmap = "Reds")    
+    ax1.imshow(Lungs1, cmap = "Greys_r")
+    floating = ax2.imshow(LungsNew, alpha = 0.5, cmap = "Reds")    
     # Use shift image to find the shift required to register the images.
     # Shift required is [-4, -357], rotation required is
     shiftRotateImage([-4, -35],357)  
+    ### interactive image registration lung 3
+    # Read data for lungs3
+    LungsNew= imread("Lungs3.jpg", as_gray = True)
 
+    #Create figure space and add subplots for shift and rotation plot
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax2 = fig.add_subplot(111)
+    # Colour images using cmap so as to view more easily on screen 
+    # Use alpha = 0.5 to make second image transparent
+    ax1.imshow(Lungs1, cmap = "Greys_r")
+    floating = ax2.imshow(LungsNew, alpha = 0.5, cmap = "Reds")    
+    #link key pressing to eventHandler function
+    fig.canvas.mpl_connect('key_press_event', eventHandler)
+    #initialise movement_storage as dictionary
+    movement_storage={"x":0,"y":0,"rotation":0}
+  
     
     # Display images
     plt.show()
     plt.close()
+    #output movement_storage dictionary when changes have been made
+    print(movement_storage)
+    #{'x': -4, 'y': -31, 'rotation': -3} which is very similar to result from before
 
 if __name__ == "__main__":
 # To allow main function to run
