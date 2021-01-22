de71dac1933add04b3f9f3b3457e469f7e995d7a
meganpaynton59@hotmail.com <meganpaynton59@hotmail.com>
2021-01-22 10:14:48 +0000

    Final commit of Assignement 2 part 1 with final changes and further comments added

--- Day4/Code/Assignment2.py
+++ Day4/Code/Assignment2.py
@@ -26,19 +26,20 @@ img2_array = img2_dcm.pixel_array
 
 # Save the overlayed figures as a png
 fig, ax = plt.subplots()
-plt.imshow(img1_array, alpha=0.5, cmap="Greys_r")
+plt.imshow(img1_array, alpha=0.5, cmap="Greys_r") # plot the images with transparency (alpha)
 plt.imshow(img2_array, alpha=0.5, cmap="Greys_r")
-plt.savefig('img1_img2_overlayed.png')
+plt.savefig('img1_img2_overlayed.png') # save the image
 plt.show()
 
+# start a new figure
 fig, ax = plt.subplots()
 ax.imshow(img1_array, alpha=0.5, cmap="Greys_r")
 img2 = ax.imshow(img2_array, alpha=0.5, cmap="Greys_r")
 
-# Function to shift specified image a specified number of shifts 
+# Function (from assignment 1 to shift specified image a specified number of shifts 
 def shiftImage(shifts, image):
     shifted_image = interpolation.shift(image, shifts)
-    return shifted_image
+    return shifted_image # return image so it can be used in a function later
 
 shifted = shiftImage([10,-20], img2_array) # 3, -25 to register the images
 img2.set_data(shifted)
@@ -47,24 +48,26 @@ plt.show()
 
 # Create a cost function which compares two images using mean squared error
 def cost(image_1, image_2):
-    N = (image_1.shape[0])*(image_1.shape[1])
-    MSE = (1/N)*sum(sum((image_1-image_2)**2))
+    N = (image_1.shape[0])*(image_1.shape[1]) # calculate number of pixels to calculate mean squared error
+    MSE = (1/N)*sum(sum((image_1-image_2)**2)) # calculate mean squared error
     return MSE
 
-print(cost(img1_array, img2_array))
+print(cost(img1_array, img1_array)) # check this MSE is 0 - it should be when comparing the same image
+print(cost(img1_array, img2_array)) # check the MSE is working for these two images
 
 # Write a function called register images to evaluate the cost function between two images
 def registerImages(shift, fixed, floating):
-    shifted_image = shiftImage(shift, floating)
-    cost_value = cost(fixed, shifted_image)
+    shifted_image = shiftImage(shift, floating) # shift image to register the image based on the optimum we found earlier
+    cost_value = cost(fixed, shifted_image) # identify the cost value based on the registered images
     return cost_value
 
-print(registerImages([1, -25], img1_array, img2_array))
+print(registerImages([3, -25], img1_array, img2_array)) # print the cost value for the registered image1 and image2
 
-# Run optomiser brute to identify the shifts for image registration, optimum shifts are 2.99566717 -25.00354501
+# Run optomiser brute to identify the optimum shifts for image registration, optimum shifts are 2.99566717 -25.00354501
 registering_shift = brute(registerImages, ((-100, 100),(-100, 100)), args=(img1_array, img2_array))
-print(registering_shift)
+print(registering_shift) # print the optimum shifts to register the images
 
+# start a new figure
 fig, ax = plt.subplots()
 ax.imshow(img1_array, alpha=0.5, cmap="Greys_r")
 img2 = ax.imshow(img2_array, alpha=0.5, cmap="Greys_r")
@@ -86,7 +89,7 @@ img4_array = img4_dcm.pixel_array
 floating_list = [img2_array, img3_array, img4_array]
 
 # Loop over floating images to produce registrations (and save them after the first time to speed up the process after)
-if os.path.exists("registrations.npy"):
+if os.path.exists("registrations.npy"): # if this loop has already been run the registrations.npy will be loaded from the files
     registrations = np.load("registrations.npy")
 else:
     registrations = []
@@ -96,19 +99,19 @@ else:
     print(registrations)
     
     registrations = np.array(registrations)
-    np.save("registrations.npy", registrations)
+    np.save("registrations.npy", registrations) # save registrations to a numpy array for use next time the script is run
 
-# loop over floating images (and shiftImage function) to produce registered images) 
+# loop over floating images (and shiftImage function) to produce registered images
 shifted_images = []
-for i, floating in enumerate(floating_list):
+for i, floating in enumerate(floating_list): # loop through floating list
     print(i)
-    registered_images = shiftImage(registrations[i], floating)
-    shifted_images.append(registered_images)
-print(shifted_images[2].shape)
+    registered_images = shiftImage(registrations[i], floating) # each "floating" will loop through registrations
+    shifted_images.append(registered_images) # append each floating image to the end of the array
+print(shifted_images[2].shape) # print registered image 1 and image 4
 
 #View registered image 1 and image 4.
 fig, ax = plt.subplots(1,1)
-plt.title("registered")
-ax.imshow(img1_array, alpha=0.5, cmap="Greens_r")
-ax.imshow(shifted_images[2], alpha=0.5, cmap="Purples_r")
+plt.title("Registered Images")
+ax.imshow(img1_array, alpha=0.5, cmap="Greens_r") 
+ax.imshow(shifted_images[2], alpha=0.5, cmap="Purples_r") # use greens and purples as these help the eyes see more of the image
 plt.show()
