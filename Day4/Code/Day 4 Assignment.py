5e80ba6e108525bff94cf8565e1aaa5dc83fce92
jj-l98 <71712407+jj-l98@users.noreply.github.com>
2021-01-22 13:06:29 +0000

    Update Day 4 Assignment.py

--- Day4/Code/Day 4 Assignment.py
+++ Day4/Code/Day 4 Assignment.py
@@ -17,3 +17,36 @@ import skimage
 from skimage import io
 import io 
 import matplotlib.image as mpimg 
+import pydicom 
+import scipy.optimize
+
+#Set working directory
+os.chdir("C:/Users/jaden/bioinformatics-course/code/ManchesterBioinformaticsCourse_Student/Day4/Code")
+os.getcwd()
+
+###Load images
+#image1_dcm = skimage.io.imread("IMG-0004-00001.dcm", as_gray=True)
+#image2_dcm = skimage.io.imread("IMG-0004-00002.dcm", as_gray=True)
+#image3_dcm = skimage.io.imread("IMG-0004-00003.dcm", as_gray=True)
+#image4_dcm = skimage.io.imread("IMG-0004-00004.dcm", as_gray=True)
+#
+#plt.imshow(image1_dcm)
+#plt.imshow(image2_dcm)
+#plt.imshow(image3_dcm)
+#plt.imshow(image4_dcm)
+
+#Load and Convert to numpy array
+image1_array = pydicom.dcmread("IMG-0004-00001.dcm").pixel_array #returns DICOM object and inside is a pixel array
+image2_array=pydicom.dcmread("IMG-0004-00002.dcm") pixel_array
+image3_array=pydicom.dcmread("IMG-0004-00003.dcm")
+image3_array=pydi
+#when the above is run- should show us the image that is stored inside the DICOM
+
+#Then plot these pixel arrays
+fig2 = plt.figure()
+ax = fig2.add_subplot(111)
+ax.imshow(image1_array, cmap="Greys_r")
+plt.show()
+
+#this returns a file, with a DICOM header, number of frames, no. of rows, no. of columns.
+#Returns image shown in .dcm file
