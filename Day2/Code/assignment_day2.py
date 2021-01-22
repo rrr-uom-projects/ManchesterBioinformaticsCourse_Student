6168c10ebe3e33e2c8c72a1cc9e24c5d59b99fd9
ed3497 <ed.otoole34@gmail.com>
2021-01-22 16:08:35 +0000

    final commit day1/2

--- Day2/Code/assignment_day2.py
+++ Day2/Code/assignment_day2.py
@@ -19,12 +19,16 @@ os.chdir(newPath)
 csvFiles = os.listdir(newPath)
 #load file names of all the csv files
 
+breathDataList = []
 #TODO: use this to load all files 
-#for cFile in csvFiles:
-#    with open(cFile, mode = "r") as f:
+for cFile in csvFiles:
+    with open(cFile, mode = "r") as f:
+        breathDataList.append(np.loadtxt(cFile, delimiter = ","))
+        
 breathData = np.loadtxt("p01.csv", delimiter = ",")
 #shape of array, p01 is (2,1107) (2 rows and 1107 columns)
 print(np.shape(breathData))
+
 
 #commented out to stop plotting
 #plt.plot(breathData)
@@ -32,6 +36,33 @@ print(np.shape(breathData))
 
 #plt.scatter(x = breathData[0,:], y = breathData[1,:])
 #plt.show()
+
+#This runs through the breathDataList and plots each plot
+#on its own axes. Probably a more elegant way to do this using subplot
+for i in range(len(breathDataList)):
+    xValues = breathDataList[i][0, :]
+    yValues = breathDataList[i][1,:]
+
+#This code was copied from the jupyter notebook and convolves the fucntion to get rid of some noise
+    N = xValues.shape[0]/30 # Try changing the window width to see the effect on the filtered signal
+    window = np.ones(int(N))
+    convolved = np.convolve(window/window.sum(), yValues, mode='same')# Note - divide by the sum of the window to 
+
+#This finds x values and the respective y values (those that are over 0) are in a dictionary
+    peaks, peakProperties = fp(convolved, height = 0) 
+
+#Plots all 3 graphs                                                           
+    plt.plot(xValues, yValues, label = "True values")
+    plt.plot(xValues, convolved, linestyle = "--", color = "r", label = "Convolved values")
+    plt.scatter(x = peaks, y = peakProperties["peak_heights"], s = 50, color = "g", label = "Peaks")
+    plt.xlabel("Time")
+    plt.ylabel("Litres per second")
+    plt.legend(loc = 4)
+    plt.title("Spirometry results {}".format(str(csvFiles[i][:-4])))
+
+    plt.show()
+
+
 
 xValues = breathData[0, :]
 yValues = breathData[1,:]
