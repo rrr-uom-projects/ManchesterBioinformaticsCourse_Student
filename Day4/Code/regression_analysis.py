6279cce879664c10980a3bcf3c39d2aeb70969e7
Viveck Kingsley <viveckkingsley@Vivecks-MacBook-Pro.local>
2021-01-22 19:37:32 +0000

    Part 2 of Day 4 Assignment DONE

--- Day4/Code/regression_analysis.py
+++ Day4/Code/regression_analysis.py
@@ -1,32 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 21 20:41:07 2021
+Assignment Part 2 (Day 4)
+_________________________
 
-@author: viveckkingsley
+Viveck Kingsley - 10811867
+
+Owen Williams - 10806830
+
+_________________________
+
 """
-
+# Import Libraries
 import matplotlib.pyplot as plt
 import numpy as np
-from skimage import io
 from scipy.ndimage import interpolation
-from scipy import optimize 
-from scipy.optimize import brute
 import pydicom
 
+# Loading Images and Converting each to a pixel_array
 patient_image = pydicom.dcmread("/Users/viveckkingsley/ManchesterBioinformaticsCourse_Student/Day4/Code/IMG-0004-00001.dcm").pixel_array
 patient_image2 = pydicom.dcmread("/Users/viveckkingsley/ManchesterBioinformaticsCourse_Student/Day4/Code/IMG-0004-00002.dcm").pixel_array
 patient_image3 = pydicom.dcmread("/Users/viveckkingsley/ManchesterBioinformaticsCourse_Student/Day4/Code/IMG-0004-00003.dcm").pixel_array
 patient_image4 = pydicom.dcmread("/Users/viveckkingsley/ManchesterBioinformaticsCourse_Student/Day4/Code/IMG-0004-00004.dcm").pixel_array
 
+# Loading the registrations.npy array
 registrations = np.load("/Users/viveckkingsley/ManchesterBioinformaticsCourse_Student/Day4/Code/registrations.npy")
 
 
 def shift_image(shifts, image):
      shifting = interpolation.shift(image, (shifts), mode="nearest")
      return shifting
-print (registrations[0])
 
+
+"""
+An open list called "shifted_images" was created. 
+The floating_list is a nested list with each element representing each floating image, as previously done in part 1.
+A loop was created to shift each image in the floating_list in line with the fixed image. This is in accordance to optimum registrations (registrations) described in part 1. 
+For each loop, 'i' increases by 1, which allow the loop to go through each element in the 'floating_list'. 
+With each loop, the new image array information is stored as a nested array in the 'shifted_images' list. 
+"""
 shifted_images=[]
 floating_list=[patient_image2, patient_image3, patient_image4]
 i=0
@@ -36,12 +48,12 @@ for image in floating_list:
     i+=1
 print(shifted_images)
 
+"""
 
-
-#for i in range[1:4]:
-#    shifted = shift_image(floating_list[i], registrations[i])
-#    shifted_images.append(shifted)
-#print(shifted_images)
+XVI:
+Using the code below, a figure containing a 2x2 plot was constructed, showing each imported image (different stages of treatment) with colourmap Greys_r as each plot of this grid. 
+    
+"""
 
 fig = plt.figure()
 
@@ -55,6 +67,17 @@ ax2.imshow(shifted_images[0], cmap="Greys_r")
 ax3.imshow(shifted_images[1], cmap="Greys_r")
 ax4.imshow(shifted_images[2], cmap="Greys_r")
 plt.show()
+
+"""
+____________________________________________
+Start of interface.py code - adapted to accept assignment code by adding in the first image of the timeseries, which attaches the interface to the figure.
+____________________________________________
+This code was used to put an interactive clickable and movable red box on the patient_image (first image), where the user can use this interface to drag the red box to the ROI (the tumour)
+and enlarge or shrink the box using arrow keys to contain the tumour within the box. The red box will be used to get indices in the pixel data array which define the region of interest, 
+much like coordinates. Using the red box defined ROI, these indices are used for each patient_image showing different stages of the tumour treatment at the same location (ROI), and plot
+each image on a 2x2 plot, to observe the image from a close-up perspective. 
+
+"""
 
 # Import the bit of matplotlib that can draw rectangles
 import matplotlib.patches as patches
@@ -179,37 +202,28 @@ plt.show()
 indices = [int(rect.get_y()), int(rect.get_y() + rect.get_height()), int(rect.get_x()), int(rect.get_x() + rect.get_width())]
 print(indices)
 
-## End of interface.py
-#floating_list1 = [patient_image2, patient_image3, patient_image4, patient_image]
-#roi=[]
-#i=0
-#for i in range(len(floating_list1)):
- #   box = (shifted_images[i] [indices[0]:indices[1], indices[2]:indices[3]])
-  #  roi.append(box)
-   # i+=1
+"""
+____________________________________________
+End of interface.py code  
+____________________________________________
+
+"""
+
+"""
+XX:
+The interface was used to select the region of interest to be analysed, using the indices variable, containing the x and y axis of the ROI. The ROI was extracted using the code below, and
+using the indices collected by patient_image (image 1 in the timeseries) the same indices were applied to the remaining images.
+"""
 
 roi = patient_image[indices[0]:indices[1], indices[2]:indices[3]]
 roi1 = shifted_images[0][indices[0]:indices[1], indices[2]:indices[3]]
 roi2 = shifted_images[1][indices[0]:indices[1], indices[2]:indices[3]]
 roi3 = shifted_images[2][indices[0]:indices[1], indices[2]:indices[3]]
 
-def mean_squared (fixed, floating):
-    return np.mean((fixed - floating)**2)
-cost = mean_squared(roi, roi)
-cost1 = mean_squared(roi, roi1)
-cost2 = mean_squared(roi, roi2)
-cost3 = mean_squared(roi, roi3)
-print (cost, cost1, cost2, cost3)
-
 """
-Using the mean_squared function, the differences between images (roi, roi1, roi2, roi3) compared to the roi original demonstrates the progression of the disease.
-cost compares roi against roi, which is 0 - this is our control 
-cost1 compares roi1 against roi, roi being the first picture of regression gave a means_squared = 46.318
-cost2 compares roi2 against roi, roi being the second picture of regression, gave a means_squared = 126.129
-cost3 compares roi3 against roi, roi being the last image of regression, gave a means_squared = 452.088
-ROI being increasingly different from the original shows that there is a progressive change that is taking place in the images.   
+XXI:
+Another 2x2 plot is made to showcase each roi variable mentioned above. 
 """
-
 fig = plt.figure()
 
 ax1=fig.add_subplot(221)
@@ -227,6 +241,28 @@ ax4.imshow(roi3, cmap="Greys_r")
 ax4.set_title("Stage 4: Lung Tumour")
 plt.show()
 
+"""
+XXII: 
+To come up with a metric that will describe the way the tumour is disappearing inside the ROI, we used the mean_squared function created in part 1. 
+Using the mean_squared function, the differences between images (roi, roi1, roi2, roi3) compared to the roi original demonstrates the progression of the disease.
+cost compares roi against roi, which is 0 - this is our control 
+cost1 compares roi1 against roi, roi being the first picture of regression gave a means_squared = 46.318
+cost2 compares roi2 against roi, roi being the second picture of regression, gave a means_squared = 126.129
+cost3 compares roi3 against roi, roi being the last image of regression, gave a means_squared = 452.088
+ROI being increasingly different from the original shows that there is a progressive change that is taking place in the images.   
+"""
+def mean_squared (fixed, floating):
+    return np.mean((fixed - floating)**2)
+cost = mean_squared(roi, roi)
+cost1 = mean_squared(roi, roi1)
+cost2 = mean_squared(roi, roi2)
+cost3 = mean_squared(roi, roi3)
+print (cost, cost1, cost2, cost3)
+
+"""
+A scatter graph was produced to display the correlation between cost and image number in the timeseries. 
+"""
+
 x_values = ["ROI", "ROI[1]", "ROI[2]", "ROI[3]"]
 y_values = [0, 46.318, 126.129, 452.055]
 
@@ -242,8 +278,9 @@ plt.savefig("/Users/viveckkingsley/ManchesterBioinformaticsCourse_Student/Day4/C
 
 plt.show()
 
-
-# XXIII: When a different ROI was used for the metric plot, all the values were the same in ROIs, as there was difference in pixel intensity. 
+"""
+XXIII: When a different ROI was used for the metric plot, all the values were the same in ROIs, as there was no difference in pixel intensity. 
+"""
 
 
 
